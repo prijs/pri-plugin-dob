aacf72f58ca742501571f41ae513f63b8cdee198
ascoders <576625322@qq.com>
2018-04-04 22:47:35 +0800

    update

--- tests/index.ts
+++ tests/index.ts
@@ -1,9 +1,5 @@
 import test from "ava"
 
-function sum(a: number, b: number) {
-  return a + b
-}
-
-test("adds 1 + 2 to equal 3", t => {
-  t.true(sum(1, 2) === 3)
+test("Example", t => {
+  t.true(true)
 })
