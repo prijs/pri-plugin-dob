1a4493f7f9eb1cfa913d7a721ddc14bbed20269f
ascoders <576625322@qq.com>
2019-02-03 13:35:46 +0800

    add plugin name

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export const getPlugin = () => import('./plugin');
 
 export const getUIPlugins = () => [import('./web-menu'), import('./web-store')];
+
+export const getConfig = () => ({
+  name: 'pri-plugin-dob'
+});
